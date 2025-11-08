f54f61a630b878c8bf21b92baa0654986f384d25
Evenaut7 <valentinolaveggi@outlook.com>
2025-11-08 19:53:06 -0300

    Reportes en Blazor

---
+++ BlazorServer/wwwroot/app.js
@@ -0,0 +1,11 @@
+﻿window.downloadFile = function (byteArray, fileName) {
+    const blob = new Blob([new Uint8Array(byteArray)], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+};
